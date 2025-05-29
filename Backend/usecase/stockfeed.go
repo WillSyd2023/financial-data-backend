43ea2f3b291d570b2f85992bced01c225ef2f5b5
William 'William <wi1123.syd@gmail.com>
2025-05-29 14:52:49 +0700

    feat: URL for GetSymbols to fetch data from Alpha Vantage

--- Backend/usecase/stockfeed.go
+++ Backend/usecase/stockfeed.go
@@ -3,6 +3,8 @@ package usecase
 import (
 	"Backend/dto"
 	"Backend/repo"
+	"fmt"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,5 +24,11 @@ func NewUsecase(rp repo.RepoItf) *Usecase {
 }
 
 func (uc *Usecase) GetSymbols(ctx *gin.Context, req *dto.GetSymbolsReq) {
-
+	// Retrieve data from Alpha Vantage API
+	url := fmt.Sprintf("https://www.alphavantage.co/"+
+		"query?function=SYMBOL_SEARCH"+
+		"&keywords=%s&apikey=%s",
+		req.Prefix,
+		os.Getenv("ALPHA_VANTAGE_API_KEY"),
+	)
 }
