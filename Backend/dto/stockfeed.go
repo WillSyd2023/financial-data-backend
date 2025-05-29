62e11959479ddcaf5520558fa0304b6f1047b335
William 'William <wi1123.syd@gmail.com>
2025-05-29 18:15:03 +0700

    feat: response structs for GetSymbols

--- Backend/dto/stockfeed.go
+++ Backend/dto/stockfeed.go
@@ -3,3 +3,13 @@ package dto
 type GetSymbolsReq struct {
 	Prefix string `json:"prefix" binding:"required,gte=1"`
 }
+
+type SymbolRes struct {
+	Symbol string `json:"1. symbol"`
+	Name   string `json:"2. name"`
+	Region string `json:"4. region"`
+}
+
+type GetSymbolsRes struct {
+	BestMatches []SymbolRes `json:"bestMatches"`
+}
