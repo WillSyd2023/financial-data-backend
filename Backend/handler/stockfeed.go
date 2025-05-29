c6db0dda98c4a34bd4b73a959d23910384ddd155
William 'William <wi1123.syd@gmail.com>
2025-05-29 19:02:26 +0700

    feat: finished GetSymbols handler

--- Backend/handler/stockfeed.go
+++ Backend/handler/stockfeed.go
@@ -4,6 +4,7 @@ import (
 	"Backend/constant"
 	"Backend/dto"
 	"Backend/usecase"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -33,5 +34,31 @@ func (hd *Handler) GetSymbols(ctx *gin.Context) {
 	req.Prefix = keywords
 
 	// usecase
-	hd.uc.GetSymbols(ctx, &req)
+	symbols, err := hd.uc.GetSymbols(ctx, &req)
+	if err != nil {
+		ctx.Error(err)
+		return
+	}
+
+	// process response before returning
+	var GetSymbolsRes dto.GetSymbolsRes
+	GetSymbolsRes.BestMatches = make([]dto.GetSymbolsSingle, 0)
+	for _, symbol := range symbols.BestMatches {
+		var singleRes dto.GetSymbolsSingle
+		singleRes.Symbol = symbol.Symbol
+		singleRes.Name = symbol.Name
+		singleRes.Region = symbol.Region
+		GetSymbolsRes.BestMatches = append(
+			GetSymbolsRes.BestMatches,
+			singleRes,
+		)
+	}
+
+	// return response
+	ctx.JSON(http.StatusOK,
+		gin.H{
+			"message": nil,
+			"error":   nil,
+			"data":    GetSymbolsRes,
+		})
 }
