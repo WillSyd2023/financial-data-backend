8dd225a44fd4b32422da7c315649e362b6a4e85c
William 'William <wi1123.syd@gmail.com>
2025-05-29 16:28:11 +0700

    feat: complete server implementation (except with incomplete handler)

--- Backend/main.go
+++ Backend/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"Backend/db"
+	"Backend/handler"
+	"Backend/middleware"
+	"Backend/repo"
+	"Backend/usecase"
 	"log"
+	"net/http"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
@@ -14,4 +21,26 @@ func main() {
 		log.Fatalf("error connect DB: %s", err)
 	}
 	defer db.Close()
+
+	// Setup server and middleware
+	r := gin.Default()
+	middleware := middleware.NewMiddleware()
+	r.Use(middleware.Error())
+
+	// Setup app (in layers)
+	rp := repo.NewRepo(db)
+	uc := usecase.NewUsecase(rp)
+	hd := handler.NewHandler(uc)
+
+	// Get synbols
+	r.GET("/symbols", hd.GetSymbols)
+
+	// Run server
+	srv := &http.Server{
+		Addr:    os.Getenv("SERVER_PORT"),
+		Handler: r.Handler(),
+	}
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("listen: %s\n", err)
+	}
 }
