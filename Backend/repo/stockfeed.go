6da4c9ef1fcec260cc0831a33ca4e2e1a64c0072
William 'William <wi1123.syd@gmail.com>
2025-05-30 23:03:44 +0700

    feat: finished CheckSymbolExists repo method

--- Backend/repo/stockfeed.go
+++ Backend/repo/stockfeed.go
@@ -1,8 +1,14 @@
 package repo
 
-import "database/sql"
+import (
+	"Backend/dto"
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+)
 
 type RepoItf interface {
+	CheckSymbolExists(*gin.Context, *dto.CollectSymbolReq) (bool, error)
 }
 
 type Repo struct {
@@ -13,4 +19,12 @@ func NewRepo(db *sql.DB) *Repo {
 	return &Repo{
 		db: db,
 	}
+}
+
+func (rp *Repo) CheckSymbolExists(ctx *gin.Context, req *dto.CollectSymbolReq) (bool, error) {
+	var exists bool
+	err := rp.db.QueryRow(
+		"SELECT EXISTS(SELECT 1 FROM symbols WHERE symbol = $1);",
+		req.Symbol).Scan(&exists)
+	return exists, err
 }
