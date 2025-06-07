75f04436783d66963bd043bfc430a6b9a21a1ce6
William 'William <wi1123.syd@gmail.com>
2025-06-08 00:36:47 +0700

    feat: draft for Get Symbols unit test function

--- Backend/handler/handler_test.go
+++ Backend/handler/handler_test.go
@@ -1,14 +1,38 @@
 package handler
 
-import "testing"
+import (
+	"Backend/usecase"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert"
+)
 
 func UnitTestHandlerGetSymbols(t *testing.T) {
-	/**
+	testCases := []struct {
+		name          string
+		ucSetup       func(*gin.Context) usecase.UsecaseItf
+		expectedError func(*gin.Context)
+		expectedBody  string
+	}{}
+
 	for _, tt := range testCases {
-		t.Run(tt.name, func(t *testing.T) {})
-		//given
-		//when
-		//then
+		t.Run(tt.name, func(t *testing.T) {
+			//given
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
+			r := httptest.NewRequest("GET", "/symbols", nil)
+			c.Request = r
+
+			hd := NewHandler(tt.ucSetup(c))
+
+			//when
+			hd.GetSymbols(c)
+
+			//then
+			assert.Equal(t, tt.expectedBody, w.Body.String())
+			tt.expectedError(c)
+		})
 	}
-	**/
 }
