238668adda4825e830b42cd06cf6f6fd8f19aded
William 'William <wi1123.syd@gmail.com>
2025-06-07 23:25:05 +0700

    feat: initial empty draft for handler_test

---
+++ Backend/handler/handler_test.go
@@ -0,0 +1,14 @@
+package handler
+
+import "testing"
+
+func TestHandlerGetSymbols(t *testing.T) {
+	/**
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {})
+		//given
+		//when
+		//then
+	}
+	**/
+}
