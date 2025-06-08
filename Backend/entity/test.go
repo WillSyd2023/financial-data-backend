25b0178ce074e2c5aa5fb0f8403053468a1c6994
William 'William <wi1123.syd@gmail.com>
2025-06-08 21:07:13 +0700

    feat: easy DateGenerator

---
+++ Backend/entity/test.go
@@ -0,0 +1,22 @@
+package entity
+
+import (
+	"Backend/constant"
+	"Backend/dto"
+	"time"
+)
+
+type DateGenerator time.Time
+
+func NewDateGenerator(date string) DateGenerator {
+	timeValue, _ := time.Parse(constant.LayoutISO, date)
+	return DateGenerator(timeValue)
+}
+
+func (d *DateGenerator) Current() dto.DateOnly {
+	return dto.DateOnly(*d)
+}
+
+func (d *DateGenerator) Next() dto.DateOnly {
+	return (*d).Current().AddDate(0, 0, 1)
+}
