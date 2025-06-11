d1d113b53b34858a4ec33550a64c2e8963215841
William 'William <wi1123.syd@gmail.com>
2025-06-12 00:26:24 +0700

    feat: empty draft for unit-testing usecase.ParseOHLCV

--- Backend/usecase/usecase_test.go
+++ Backend/usecase/usecase_test.go
@@ -1,1 +1,40 @@
 package usecase
+
+import (
+	"Backend/dto"
+	"Backend/repo"
+	"Backend/util"
+	"errors"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert"
+)
+
+func TestUnitUsecaseParseOHLCV(t *testing.T) {
+	testCases := []struct {
+		name           string
+		rpSetup        func(*gin.Context) repo.RepoItf
+		hcSetup        func(*gin.Context) util.HttpClientItf
+		tsInput        func() *map[string]string
+		expectedOutput dto.DailyOHLCVRes
+		expectedError  error
+	}{}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			//given
+			c, _ := gin.CreateTestContext(httptest.NewRecorder())
+			uc := NewUsecase(tt.rpSetup(c), tt.hcSetup(c))
+
+			//when
+			output, err := uc.ParseOHLCV(c, tt.tsInput())
+
+			//then
+			assert.Equal(t, reflect.DeepEqual(tt.expectedOutput, *output), true)
+			assert.Equal(t, errors.Is(tt.expectedError, err), true)
+		})
+	}
+}
