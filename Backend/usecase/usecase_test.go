a2456880777cba4920ba29db48eaa4933fbb8774
William 'William <wi1123.syd@gmail.com>
2025-06-12 00:53:24 +0700

    feat: first unit test for usecase.ParseOHLCV

--- Backend/usecase/usecase_test.go
+++ Backend/usecase/usecase_test.go
@@ -1,12 +1,12 @@
 package usecase
 
 import (
+	"Backend/constant"
 	"Backend/dto"
-	"Backend/repo"
-	"Backend/util"
+	mocks1 "Backend/mocks/repo"
+	mocks2 "Backend/mocks/util"
 	"errors"
 	"net/http/httptest"
-	"reflect"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -16,24 +16,36 @@ import (
 func TestUnitUsecaseParseOHLCV(t *testing.T) {
 	testCases := []struct {
 		name           string
-		rpSetup        func(*gin.Context) repo.RepoItf
-		hcSetup        func(*gin.Context) util.HttpClientItf
 		tsInput        func() *map[string]string
-		expectedOutput dto.DailyOHLCVRes
+		expectedOutput func(*dto.DailyOHLCVRes)
 		expectedError  error
-	}{}
+	}{
+		{
+			name: "no open price",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				assert.Equal(t, nil, output)
+			},
+			expectedError: constant.ErrAlphaParseBody(
+				"can't find open price as usual",
+			),
+		},
+	}
 
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
 			//given
 			c, _ := gin.CreateTestContext(httptest.NewRecorder())
-			uc := NewUsecase(tt.rpSetup(c), tt.hcSetup(c))
+			uc := NewUsecase(new(mocks1.RepoItf), new(mocks2.HttpClientItf))
 
 			//when
 			output, err := uc.ParseOHLCV(c, tt.tsInput())
 
 			//then
-			assert.Equal(t, reflect.DeepEqual(tt.expectedOutput, *output), true)
+			tt.expectedOutput(output)
 			assert.Equal(t, errors.Is(tt.expectedError, err), true)
 		})
 	}
