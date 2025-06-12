138eea66b91853ccc9c6b0afc9fbb95d9aed8586
William 'William <wi1123.syd@gmail.com>
2025-06-12 21:33:40 +0700

    feat: finished unit testing for usecase.ParseOHLCV

--- Backend/usecase/usecase_test.go
+++ Backend/usecase/usecase_test.go
@@ -8,11 +8,13 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/assert"
+	"github.com/shopspring/decimal"
 )
 
 func TestUnitUsecaseParseOHLCV(t *testing.T) {
@@ -60,6 +62,212 @@ func TestUnitUsecaseParseOHLCV(t *testing.T) {
 					),
 					true,
 				)
+			},
+		},
+		{
+			name: "no high price",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				ts["1. open"] = "100"
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				assert.Equal(t, nil, output)
+			},
+			expectedError: func(err error) {
+				expected := constant.ErrAlphaParseBody(
+					"can't find high price as usual",
+				)
+				assert.Equal(t, errors.Is(expected, err), true)
+			},
+		},
+		{
+			name: "unparseable high price",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				ts["1. open"] = "100"
+				ts["2. high"] = "one hundred"
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				assert.Equal(t, nil, output)
+			},
+			expectedError: func(err error) {
+				var ce constant.CustomError
+				assert.Equal(t, errors.As(err, &ce), true)
+				assert.Equal(t, ce.StatusCode, http.StatusBadGateway)
+				assert.Equal(
+					t,
+					strings.HasPrefix(
+						ce.Message,
+						"Alpha Vantage API response-body-parse error: ",
+					),
+					true,
+				)
+			},
+		},
+		{
+			name: "no low price",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				ts["1. open"] = "100"
+				ts["2. high"] = "100"
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				assert.Equal(t, nil, output)
+			},
+			expectedError: func(err error) {
+				expected := constant.ErrAlphaParseBody(
+					"can't find low price as usual",
+				)
+				assert.Equal(t, errors.Is(expected, err), true)
+			},
+		},
+		{
+			name: "unparseable low price",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				ts["1. open"] = "100"
+				ts["2. high"] = "100"
+				ts["3. low"] = "one hundred"
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				assert.Equal(t, nil, output)
+			},
+			expectedError: func(err error) {
+				var ce constant.CustomError
+				assert.Equal(t, errors.As(err, &ce), true)
+				assert.Equal(t, ce.StatusCode, http.StatusBadGateway)
+				assert.Equal(
+					t,
+					strings.HasPrefix(
+						ce.Message,
+						"Alpha Vantage API response-body-parse error: ",
+					),
+					true,
+				)
+			},
+		},
+		{
+			name: "no close price",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				ts["1. open"] = "100"
+				ts["2. high"] = "100"
+				ts["3. low"] = "100"
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				assert.Equal(t, nil, output)
+			},
+			expectedError: func(err error) {
+				expected := constant.ErrAlphaParseBody(
+					"can't find close price as usual",
+				)
+				assert.Equal(t, errors.Is(expected, err), true)
+			},
+		},
+		{
+			name: "unparseable close price",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				ts["1. open"] = "100"
+				ts["2. high"] = "100"
+				ts["3. low"] = "100"
+				ts["4. close"] = "one hundred"
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				assert.Equal(t, nil, output)
+			},
+			expectedError: func(err error) {
+				var ce constant.CustomError
+				assert.Equal(t, errors.As(err, &ce), true)
+				assert.Equal(t, ce.StatusCode, http.StatusBadGateway)
+				assert.Equal(
+					t,
+					strings.HasPrefix(
+						ce.Message,
+						"Alpha Vantage API response-body-parse error: ",
+					),
+					true,
+				)
+			},
+		},
+		{
+			name: "no volume",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				ts["1. open"] = "100"
+				ts["2. high"] = "100"
+				ts["3. low"] = "100"
+				ts["4. close"] = "100"
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				assert.Equal(t, nil, output)
+			},
+			expectedError: func(err error) {
+				expected := constant.ErrAlphaParseBody(
+					"can't find volume as usual",
+				)
+				assert.Equal(t, errors.Is(expected, err), true)
+			},
+		},
+		{
+			name: "unparseable volume",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				ts["1. open"] = "100"
+				ts["2. high"] = "100"
+				ts["3. low"] = "100"
+				ts["4. close"] = "100"
+				ts["5. volume"] = "one hundred"
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				assert.Equal(t, nil, output)
+			},
+			expectedError: func(err error) {
+				var ce constant.CustomError
+				assert.Equal(t, errors.As(err, &ce), true)
+				assert.Equal(t, ce.StatusCode, http.StatusBadGateway)
+				assert.Equal(
+					t,
+					strings.HasPrefix(
+						ce.Message,
+						"Alpha Vantage API response-body-parse error: ",
+					),
+					true,
+				)
+			},
+		},
+		{
+			name: "completely parseable",
+			tsInput: func() *map[string]string {
+				ts := make(map[string]string)
+				ts["1. open"] = "100"
+				ts["2. high"] = "100"
+				ts["3. low"] = "100"
+				ts["4. close"] = "100"
+				ts["5. volume"] = "100"
+				return &ts
+			},
+			expectedOutput: func(output *dto.DailyOHLCVRes) {
+				var ohlcv dto.DailyOHLCVRes
+				ohlcv.OHLC = make(map[string]decimal.Decimal)
+				ohlcv.OHLC["open"] = decimal.NewFromInt(100)
+				ohlcv.OHLC["high"] = decimal.NewFromInt(100)
+				ohlcv.OHLC["low"] = decimal.NewFromInt(100)
+				ohlcv.OHLC["close"] = decimal.NewFromInt(100)
+				ohlcv.Volume = 100
+
+				assert.Equal(t, reflect.DeepEqual(&ohlcv, output), true)
+			},
+			expectedError: func(err error) {
+				assert.Equal(t, nil, err)
 			},
 		},
 	}
